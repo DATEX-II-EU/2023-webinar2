2aaf82ca329018eb8f7efb864db78c917493446c
Jonas Jäderberg <jonas@jaderb.se>
2023-10-29 10:23:22 +0100

    Step2

--- DATEXSnaphotPull/Program.cs
+++ DATEXSnaphotPull/Program.cs
@@ -1,6 +1,10 @@
+using DATEXSnaphotPull.Controllers;
+using DATEXSnapshotPull;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddScoped<IDatexController,MyController>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
